23440c98e9deadb1d158eb9ef33fa8baa1f49fc3
ovenfosten <ovenfosten@gmail.com>
2025-03-10 19:40:08 +0800

    Create initial resolve alg

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include"Resolve.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto data = resolve();
+    for (const auto &i : data) {
+        std::cout << i.course_name << std::endl;
+        std::cout << i.course_collage << std::endl;
+        std::cout << i.course_credits << std::endl;
+        std::cout << i.course_routine << std::endl;
+        std::cout << i.course_date << std::endl;
+        std::cout << i.course_start_time << std::endl;
+        std::cout << i.course_end_time << std::endl;
+        std::cout << i.course_teachers << std::endl;
+        std::cout << i.course_location << std::endl;
+        std::cout << i.bool_if_lab_course << std::endl;
+        std::cout << "----------------------" << std::endl;
+    }
 }
